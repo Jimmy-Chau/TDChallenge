8f30e3b90644fd07abf94b5b73782600bceaa1e8
Jimmy-Chau <chau_jim@hotmail.com>
2015-11-16 14:50:41 -0500

    - Added/Updated Register and Login

--- app/src/main/java/com/example/adamfischer/jimmychau/tdchallenge/LoginActivity.java
+++ app/src/main/java/com/example/adamfischer/jimmychau/tdchallenge/LoginActivity.java
@@ -1,16 +1,81 @@
 package com.example.adamfischer.jimmychau.tdchallenge;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+    Button btnLogin,btnRegister;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get The Refference Of Buttons
+        btnLogin=(Button)findViewById(R.id.loginButton);
+        btnRegister=(Button)findViewById(R.id.registerButton);
+
+        // Set OnClick Listener on SignUp button
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentRegister = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
+    }
+
+    // Method to handleClick Event of Sign In Button
+    public void logIn(View V)
+    {
+        // get the Refferences of views
+        final EditText editTextUserName=(EditText)findViewById(R.id.usernameEditText);
+        final EditText editTextPassword=(EditText)findViewById(R.id.passwordEditText);
+
+        Button btnSignIn=(Button)findViewById(R.id.loginButton);
+
+        // Set On ClickListener
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // get The User name and Password
+                String userName = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // fetch the Password form database for respective user name
+                String storedPassword = loginDataBaseAdapter.getSinlgeEntry(userName);
+
+                // check if the Stored password matches with  Password entered by user
+                if (password.equals(storedPassword)) {
+                    Toast.makeText(LoginActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
     }
 
     @Override
