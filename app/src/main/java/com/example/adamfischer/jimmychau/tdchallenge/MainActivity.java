fe5481b1deb9489260c5fc2f9e02d39aab71af52
AdamJFischer <adamfischer@live.ca>
2015-11-16 19:33:30 -0500

    Added swipe views for main activity

--- app/src/main/java/com/example/adamfischer/jimmychau/tdchallenge/MainActivity.java
+++ app/src/main/java/com/example/adamfischer/jimmychau/tdchallenge/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.adamfischer.jimmychau.tdchallenge;
 
 import android.app.Activity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+//public class MainActivity extends Activity {
 public class MainActivity extends Activity {
+
+    // Data about logged in user
+    UserData userData;
+
+    // Tabs
+    private final String[] tabTitles = {
+            "My Projects",
+            "Other Projects",
+            "Profile"
+    };
+
+    private PagerAdapter pagerAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pagerAdapter = new PagerAdapter(getFragmentManager());
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
     }
 
     @Override
@@ -33,5 +60,69 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /* classes for working with tab views */
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            System.out.println("Showing fragment " + position);
+            switch (position) {
+                case 0: return new MyProjectsFragment();
+                case 1: return new OtherProjectsFragment();
+                case 2: return new ProfileFragment();
+                default:
+                    System.err.println("No fragment found for position " + position);
+                    return new MyProjectsFragment(); // go to main page
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+    }
+
+    public static class MyProjectsFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my_projects, container, false);
+
+            return rootView;
+        }
+
+
+    }
+
+    public static class OtherProjectsFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_other_projects, container, false);
+
+
+
+            return rootView;
+        }
+    }
+
+    public static class ProfileFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+            return rootView;
+        }
     }
 }
